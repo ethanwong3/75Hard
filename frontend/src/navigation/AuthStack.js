ce2abc77283efbadbd632443b0c2e1091d2a030d
Ethan <ethaneswanebban@gmail.com>
2025-02-05 22:29:38 +1100

    committing to move branches

--- frontend/src/navigation/AuthStack.js
+++ frontend/src/navigation/AuthStack.js
@@ -1,4 +1,3 @@
-// frontend/src/navigation/AuthStack.js
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
