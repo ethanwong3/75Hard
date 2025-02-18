a50aa5f740ad92356448fe67a5b5f95b4b48fa24
Ethan <ethaneswanebban@gmail.com>
2025-02-18 23:40:14 +1100

    draft maybe final challenge schema

---
+++ backend/models/Challenge.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const challengeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    rules: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    total: {
+      type: Number,
+      min: 1,
+      required: true,
+    },
+    public: {
+      type: Boolean,
+      default: false,
+    },
+    difficulty: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("challenge", challengeSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,3 @@
-import mongoose from "mongoose";
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
